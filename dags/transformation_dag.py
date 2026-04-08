2ffc4367d7a67cfac7796d24d96b8943528b8644
hafida djelti <h.djelti@laposte.net>
2026-04-08 16:25:13 +0200

    feat(dag/trasnformation_dag.py): Ajout du DAG de transformation + fonction run_transformation

--- dags/transformation_dag.py
+++ dags/transformation_dag.py
@@ -11,7 +11,7 @@ default_args = {
 with DAG(
     dag_id="transformation_rfm",
     default_args=default_args,
-    start_date=datetime(2026, 1, 1),
+    start_date=datetime(2026, 1, 4),
     schedule_interval=None,
     catchup=False,
     tags=["rfm", "transformation"],
